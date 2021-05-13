00186d4287f1059c7c431c30d70f993fef45cb95
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-05-13 20:09:21 +0900

    [add] index-page

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import { withRouter } from 'react-router';
 import { SocketContext } from './sockets';
+import { nanoid } from 'nanoid';
 
 class Home extends React.Component {
   static contextType = SocketContext;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({username: event.target.value});
+  }
 
   render() {
       console.log(this.context);
@@ -14,11 +27,17 @@ class Home extends React.Component {
         <form>
           <div>
             username <br/>
-            <input id="username" />
+            <input id="username" value={this.state.username} onChange={this.handleChange}/>
           </div>
         </form>
         <Button variant="contained" color="primary" onClick={() => {
-          this.props.history.push('/waiting/test');
+          if(!this.state.username) {
+            alert('ユーザー名を入力してください');
+            return;
+          }
+          let room_id = nanoid(10);
+          this.context.emit('create', room_id, this.state.username);
+          this.props.history.push('/waiting/' + room_id);
         }}>ホストで始める</Button><br/>
         <Button variant="contained" color="primary" onClick={() => {
           this.props.history.push('/gamelist');
