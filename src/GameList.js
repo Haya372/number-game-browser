7e9befe6974e572376e38e1b2d0520e4e488f71d
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-05-13 20:48:46 +0900

    [add] gamelist-page（途中）

--- src/GameList.js
+++ src/GameList.js
@@ -1,16 +1,58 @@
 import React from 'react';
+import { SocketContext } from './sockets';
+import PeopleIcon from '@material-ui/icons/People';
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { withRouter } from 'react-router';
 
 class GameList extends React.Component {
-  /*constructor(props) {
-    super(props);
+  static contextType = SocketContext;
 
-  }*/
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: []
+    };
+  }
+
+  componentDidMount() {
+    let socket = this.context;
+    socket.on('room list', (data) => {
+      this.setState({
+        rooms: data.rooms
+      });
+    });
+
+    setTimeout(() => {
+      socket.emit('get rooms');
+    }, 100);
+  }
 
   render() {
+    let rooms = [];
+    this.state.rooms.forEach((room) => {
+      rooms.push(
+        <ListItem button key={room.room_id} onClick={() => {
+          // socket.emitの処理
+          this.props.history.push('/waiting/' + room.room_id);
+        }}>
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary={room.host} secondary={"人数：" + room.people} />
+        </ListItem>
+      )
+    });
     return (
-      <p>this is game list</p>
+      <div>
+        <h1>ゲーム一覧</h1>
+        {rooms}
+        <Button variant="contained" color="primary" onClick={() => {
+          this.context.emit('get rooms');
+        }}>リロード</Button>
+      </div>
     )
   }
 }
 
-export default GameList;
+export default withRouter(GameList);
--- src/Home.js
+++ src/Home.js
@@ -20,7 +20,6 @@ class Home extends React.Component {
   }
 
   render() {
-      console.log(this.context);
     return (
       <div className="home">
         <h1>Number Game</h1>
